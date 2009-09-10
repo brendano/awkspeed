474264d36c92f2ab6e7f9195a9afe8119ab2ccf9
David R. MacIver <david.maciver@gmail.com>
2009-09-10 14:05:04 +0100

    fix flushing in Java version

--- Formatter.java
+++ Formatter.java
@@ -4,29 +4,37 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.util.HashMap;
 public class Formatter {
-    public static void main(String[] args) throws Exception{
+		public static void main(String[] args) throws Exception{
 	HashMap<String, Integer> imap = new HashMap<String, Integer>();
 	HashMap<String, Integer> jmap = new HashMap<String, Integer>();
-        int j = 0;
+				int j = 0;
+
+	BufferedWriter vocab = new BufferedWriter(new FileWriter("vocab"));
+
 	for(String file : args){
-	    BufferedReader in = new BufferedReader(new FileReader(file));
-	    BufferedWriter out = new BufferedWriter(new FileWriter(file + "n"));
-	    BufferedWriter vocab = new BufferedWriter(new FileWriter("vocab"));
-	    String feats = in.readLine();
-	    int i = 0;
-	    while(feats != null){
+			BufferedReader in = new BufferedReader(new FileReader(file));
+			BufferedWriter out = new BufferedWriter(new FileWriter(file + "n"));
+			String feats = in.readLine();
+			int i = 0;
+			while(feats != null){
 		String[] vals = feats.split(" ");
 		String key = file + " " + vals[0];
 		if(imap.get(key) == null){
-		    imap.put(key, ++i);
+				imap.put(key, ++i);
 		}
 		if(jmap.get(vals[1]) == null){
-		    jmap.put(vals[1], ++j);
-		    vocab.write(vals[1] + "\n");
+				jmap.put(vals[1], ++j);
+				vocab.write(vals[1] + "\n");
 		}
 		out.write(imap.get(key) + " " + jmap.get(vals[1]) + " " + vals[2] + "\n");
 		feats = in.readLine();
-	    }
+			}
+	 
+		out.close();
+ 
 	}
-    }
+
+	vocab.flush();
+
+		}
 }
